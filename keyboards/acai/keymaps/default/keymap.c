e23008c97dba46c62a3944343cc8598777fe8b00
mrktix <mrktix@gmail.com>
2023-05-10 17:40:08 -0700

    pretty much finalized the keymap, moved around some symbols

--- keyboards/acai/keymaps/default/keymap.c
+++ keyboards/acai/keymaps/default/keymap.c
@@ -23,6 +23,13 @@ enum custom_layers {
     _OTH,
 };
 
+enum custom_keycodes {
+    MY_DDOT = SAFE_RANGE,
+    MY_DCOL,
+    MY_LEAD,
+    MY_E,
+};
+
 // backspace to del ... dot to comma
 const key_override_t dot_comma_override =
     ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COMMA);
@@ -36,20 +43,14 @@ const key_override_t** key_overrides = (const key_override_t*[]){
     NULL
 };
 
-enum custom_keycodes {
-    MY_DDOT = SAFE_RANGE,
-    MY_DCOL,
-    MY_LEAD,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* alphas, space, layers, shift, control, backspace, enter, tab, maybe capslock? */
+/* alphas, space, liyers, shift, control, backspace, enter, tab, maybe capslock? */
 [_ALP] = LAYOUT(
     KC_J,         KC_C,         KC_Y,         KC_F,         KC_K,           /**/          KC_Z,         KC_L,         KC_TAB,       KC_U,         KC_Q,
     KC_R,         KC_S,         KC_T,         KC_H,         KC_D,           /**/          KC_M,         KC_N,         KC_A,         KC_I,         KC_O,
     KC_ENT,       KC_V,         KC_G,         KC_P,         KC_B,           /**/          KC_X,         KC_W,         KC_DOT,       KC_SCLN,      KC_QUOT,
-          XXXXXXX,MY_LEAD,      KC_BSPC,      KC_SPC,       TO(_SYM),       /**/          TO(_NUM),     KC_E,         OSM(MOD_LSFT),OSM(MOD_LCTL),XXXXXXX
+          XXXXXXX,MY_LEAD,      KC_BSPC,      KC_SPC,       TO(_SYM),       /**/          TO(_NUM),     MY_E,         OSM(MOD_LSFT),OSM(MOD_LCTL),XXXXXXX
 ),
 
 /* numbers and math symbols */
@@ -57,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PERC,      KC_MINUS,     KC_PLUS,      KC_EQUAL,     XXXXXXX,        /**/          XXXXXXX,      KC_EQUAL,     KC_ASTERISK,  KC_SLASH,     KC_TILD,
     KC_1,         KC_2,         KC_3,         KC_4,         XXXXXXX,        /**/          XXXXXXX,      KC_5,         KC_6,         KC_7,         KC_8,
     _______,      KC_ESC,       KC_BSLS,      KC_0,         XXXXXXX,        /**/          XXXXXXX,      KC_9,         KC_DOT,       KC_COLN,      XXXXXXX,
-          XXXXXXX,_______,      _______,      _______,      _______,        /**/          _______,      TO(_ALP),     TO(_OTH),     XXXXXXX,XXXXXXX
+          XXXXXXX,_______,      _______,      _______,      _______,        /**/          _______,      TO(_ALP),     _______,      _______,XXXXXXX
 ),
 
 /* symbols: brc [],  cbr {},  abk <>,  prn (), circ ^ .. :: ? ! & | _ # $ @ ^ % \ */
 [_SYM] = LAYOUT(
-    XXXXXXX,      KC_AT,        KC_QUES,      KC_DLR,       XXXXXXX,        /**/          XXXXXXX,      KC_EQUAL,     KC_EXLM,      KC_UNDS,      XXXXXXX,
-    KC_HASH,      KC_AMPR,      KC_LCBR,      KC_RCBR,      XXXXXXX,        /**/          XXXXXXX,      KC_RPRN,      KC_LPRN,      KC_PIPE,      KC_CIRC,
-    _______,      MY_DDOT,      KC_LABK,      KC_RABK,      XXXXXXX,        /**/          XXXXXXX,      KC_RBRC,      KC_LBRC,      MY_DCOL,      XXXXXXX,
+    XXXXXXX,      KC_CIRC,      KC_QUES,      KC_DLR,       XXXXXXX,        /**/          XXXXXXX,      KC_EQUAL,     KC_EXLM,      KC_UNDS,      XXXXXXX,
+    XXXXXXX,      KC_AMPR,      KC_LCBR,      KC_RCBR,      XXXXXXX,        /**/          XXXXXXX,      KC_RPRN,      KC_LPRN,      KC_PIPE,      XXXXXXX,
+    _______,      KC_HASH,      KC_LABK,      KC_RABK,      XXXXXXX,        /**/          XXXXXXX,      KC_RBRC,      KC_LBRC,      KC_AT,        XXXXXXX,
           XXXXXXX,_______,      _______,      _______,      _______,        /**/          _______,      TO(_ALP),     TO(_OTH),     XXXXXXX,XXXXXXX
 ),
 
@@ -84,14 +85,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
         case MY_LEAD:
             if (record -> event.pressed) {
-                if (guilock) {
+                register_code(KC_LGUI);
+                register_code(KC_LALT);
+                leaderlock = true;
+            }
+            break;
+
+        case MY_E:
+            if (record -> event.pressed) {
+                if (leaderlock) {
                     unregister_code(KC_LGUI);
                     unregister_code(KC_LALT);
+                    leaderlock = false;
                 } else {
-                    register_code(KC_LGUI);
-                    register_code(KC_LALT);
+                    register_code(KC_E);
                 }
-                leaderlock = !leaderlock;
+            } else {
+                unregister_code(KC_E);
             }
             break;
 
